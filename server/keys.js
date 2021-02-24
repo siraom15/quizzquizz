681e9f9bdd621413b4e5abb06d10c26dba062fe2
Siriwat <siriwatjaiyungyuen@gmail.com>
2021-02-24 21:15:38 +0700

    [ Add ] ➕ Add dbs config

---
+++ server/keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+    MONGOURI : "mongodb+srv://quizz:quizzquizz@gocourse.wzkcz.mongodb.net/quizz?retryWrites=true&w=majority",
+    MONGOUSERNAME : "quizz",
+    MONGOPASSWORD : "quizzquizz",
+    DATABASE : "quizz",
+    
+}
