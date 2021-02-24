03a69d948317f357ae048f351c712db7026784ac
Siriwat <siriwatjaiyungyuen@gmail.com>
2021-02-24 21:16:25 +0700

    [ Update ] ➕ Add cors pkg & import router & import model

--- server/app.js
+++ server/app.js
@@ -2,19 +2,42 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require("cors");
+//keys
+let { MONGOURI } = require("./keys");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// database
+const mongoose = require('mongoose');
+//connect db
+console.log(MONGOURI);
+mongoose.connect(MONGOURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(()=>{
+        console.log("Connected");
+    })
+    .catch((error) => {
+        console.log(error);
+    })
+
+//model 
+require('./models/user')
+require('./models/questions')
+
+// router
+var questionRouter = require('./routes/questions')
+var apiRouter = require('./routes/api');
+var userRouter = require('./routes/user')
+
 
 var app = express();
-
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api', apiRouter);
+app.use('/api/questions', questionRouter);
+app.use('/api/user', userRouter);
 
 module.exports = app;
