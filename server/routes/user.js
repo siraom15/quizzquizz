6422efb6bac96401b17b8a12c1e59118f710b7d1
Siriwat <siriwatjaiyungyuen@gmail.com>
2021-02-24 21:14:43 +0700

    [ Update ] ➕ Add signup function

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,5 +1,55 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const User = mongoose.model("User");
 
+const bcrypt = require('bcrypt');
+const saltRounds = 2;
+
+router.post("/signup", (req, res, next) => {
+    const {
+        username, email, password
+    } = req.body;
+    console.log(req.body);
+    if (username && email && password ) {
+        User.find({
+            "$or": [{
+                username: username
+            }, {
+                email: email
+            }]
+        }).then(result => {
+            if (result.length > 0) {
+                res.status(422).json({
+                    "error": "Email Or Username Already taken"
+                })
+            } else {
+                bcrypt.hash(password, saltRounds, function (err, hash) {
+                    if (err) console.log(err);
+                    const user = new User({
+                        username: username,
+                        email: email,
+                        password: hash,
+                        score: 0,
+                        userType: "student",
+                    });
+                    user.save()
+                        .then(user => {
+                            res.status(200).json({
+                                "success" : "Sign up success"
+                            })
+                        }).catch(err => {
+                            console.log(err)
+                        })
+
+                });
+            }
+        })
+    }else{
+        res.status(422).json({
+            "error" : "Please fill the form"
+        })
+    }
+})
 
 module.exports = router;
