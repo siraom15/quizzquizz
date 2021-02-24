73949978e9ef4620cb7ee66a2eda7cdb2c130ef9
Siriwat <siriwatjaiyungyuen@gmail.com>
2021-02-24 23:51:05 +0700

    [ Add ] ➕ JWT pkg & JWT secret

--- server/keys.js
+++ server/keys.js
@@ -3,5 +3,5 @@ module.exports = {
     MONGOUSERNAME : "quizz",
     MONGOPASSWORD : "quizzquizz",
     DATABASE : "quizz",
-    
+    JWTSECRET : "RsP*9AWC#G!@J_PQcWZP2EZAv9jsJ7m!VGzg!7&yS3tfd8E+jC3Rqa8W7LY3Z*HnnnNuUmB^b5*7yzu4h3gv_Jq6awHvJgc4ZU=%"
 }
