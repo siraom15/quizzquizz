7a282dc67bb320d54b7ab77d5f28b4e0f2e0c580
Siriwat <siriwatjaiyungyuen@gmail.com>
2021-02-27 23:33:47 +0700

    [ Update ] ➕ add user route

--- server/routes/user.js
+++ server/routes/user.js
@@ -8,11 +8,10 @@ const controller = require('../controllers/userController');
 const checkLogin = require("../middleware/checkLogin")
 
 router.get("/", controller.indexUserPage);
-
 router.post("/signup", controller.addUser)
-
 router.post("/signin", controller.checkUser)
-
 router.get("/detail", checkLogin, controller.getUserData);
-
+router.post("/addscore", checkLogin,  controller.addScore);
+router.post("/decreasescore", checkLogin,  controller.decreaseScore);
+router.post("/resetscore", checkLogin,  controller.resetScore);
 module.exports = router;
