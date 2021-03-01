db841fdf828943c0ed42af89e91c0f8041acf3da
Siriwat <siriwatjaiyungyuen@gmail.com>
2021-03-02 01:35:19 +0700

    [ Update ] ➕ Add score controlloer in questions

--- server/controllers/questionController.js
+++ server/controllers/questionController.js
@@ -1,6 +1,7 @@
 // mongo
 const mongoose = require('mongoose');
 const Questions = mongoose.model("Questions");
+const Choices = mongoose.model("Choices")
 
 const addQuestion = (req, res, next) => {
     let { questionTitle, questionDescribe, choices, score, category } = req.body;
@@ -9,14 +10,13 @@ const addQuestion = (req, res, next) => {
             questionTitle: questionTitle,
             questionDescribe: questionDescribe,
             choices: choices,
-            userId: req.user_data._id,
+            userId: mongoose.Types.ObjectId(req.user_data._id),
             score: score,
             category: category
         })
         question.save().then(
             result => {
                 res.json({ question: result })
-
             }
         ).catch(
             err => console.log(err)
@@ -36,16 +36,77 @@ const getAllQuestion = (req, res, next) => {
 }
 const deleteQuestion = (req, res, next) => {
     const { _id = req._id } = req.body;
-    Questions.findOneAndDelete({ _id: _id, userId : req.user_data._id }).then(
+
+    Questions.findOneAndDelete({ _id: _id, userId: req.user_data._id }).then(
         res.status(200).json({
-            "success" : "Delete successfully"
+            "success": "Delete successfully"
         })
     ).catch(err => console.log(err))
 }
+const updateScore = (req, res, next) => {
+    const { score, questionId } = req.body;
+    if (score < 0) return;
+    Questions.findOneAndUpdate({ _id: questionId, userId: req.user_data._id }, { $set: { score: score } }).then(
+        data => {
+            if (data) {
+                res.status(200).json({
+                    "success": "update successfully",
+                    "question_data": data
+                })
+            } else {
+                res.status(422).json({
+                    "error": "not found question"
+                })
+            }
 
+        }
+    )
+        .catch(err => {
+            console.log(err);
+        })
+}
+const addChoices = (req, res, next) => {
+    const { choices, questionId } = req.body;
+    if (!choices || !questionId) return res.status(422).json({ "error": "Please Fill the form" })
+    console.log(choices + " " + questionId);
+    Questions.findOneAndUpdate({ _id: mongoose.Types.ObjectId(questionId), userId: mongoose.Types.ObjectId(req.user_data._id) }, { $push: { choices: choices } })
+        .then(data => {
+            if (data) {
+                res.status(200).json({
+                    "success": "Update successfully",
+                    "question_data": data
+                })
+            } else {
+                res.status(200).json({
+                    "success": "No Permission",
+                    "question_data": data
+                })
+            }
+        })
+        .catch(err => {
+            console.log(err);
+        })
+}
+const getQuestionById = (req, res,next)=>{
+    const {questionId} =req.body;
+    if(questionId){
+        Questions.findById(mongoose.Types.ObjectId(questionId)).then(
+            data =>{
+                if(data)
+                console.log(data);
+            }
+        ).catch(err=>{
+            
+        })
+    }
+
+}
 
 module.exports = {
     addQuestion,
     getAllQuestion,
-    deleteQuestion
+    deleteQuestion,
+    updateScore,
+    addChoices,
+    getQuestionById
 }
--- server/routes/questions.js
+++ server/routes/questions.js
@@ -7,8 +7,10 @@ const controller = require("../controllers/questionController")
 const checkLogin = require('../middleware/checkLogin')
 
 router.post("/addquestion", checkLogin, controller.addQuestion);
-router.get("/allquestion", checkLogin, controller.getAllQuestion);
+// router.get("/allquestion", checkLogin, controller.getAllQuestion);
+router.get("/allquestion", controller.getAllQuestion);
 router.delete("/deletequestion", checkLogin, controller.deleteQuestion);
-
-
+router.post("/updatescore", checkLogin, controller.updateScore);
+router.post("/addchoices", checkLogin, controller.addChoices);
+router.get("getquestion",checkLogin, controller.getQuestionById);
 module.exports = router;
